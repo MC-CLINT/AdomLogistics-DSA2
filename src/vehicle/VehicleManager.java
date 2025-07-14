e0c4edda5d82721bde9b8bb4958f50558a104640
Oteng <emmanueloteng.k@gmail.com>
2025-07-14 11:06:12 +0000

    feat: enhance VehicleManager with comprehensive vehicle management methods

--- src/vehicle/VehicleManager.java
+++ src/vehicle/VehicleManager.java
@@ -1,5 +1,37 @@
 package vehicle;
-
+import java.util.ArrayList;
 public class VehicleManager {
-  
+  private VehicleTree vehicleTree;
+  private ArrayList<Vehicle> vehicleList;public VehicleManager() {
+        vehicleTree = new VehicleTree();
+        vehicleList = new ArrayList<Vehicle>();
+    }
+
+    public void addVehicle(Vehicle vehicle) {
+        vehicleTree.addVehicle(vehicle);
+        vehicleList.add(vehicle);
+    }
+
+    public Vehicle searchVehicle(String regNumber) {
+        return vehicleTree.searchVehicle(regNumber);
+    }
+
+    public boolean removeVehicle(String regNumber) {
+        Vehicle toRemove = searchVehicle(regNumber);
+        if (toRemove != null) {
+            vehicleTree.removeVehicle(regNumber);
+            vehicleList.remove(toRemove);
+            return true;
+        }
+        return false;
+    }
+
+    public void displayAllVehicles() {
+        vehicleTree.displayVehicles();
+    }
+
+    public ArrayList<Vehicle> getAllVehicles() {
+        return vehicleList;
+    }
 }
+
